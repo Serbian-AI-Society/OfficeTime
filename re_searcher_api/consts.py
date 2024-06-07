73558fc343d8161b4d7cbf87751c6fbf0ad9d0c3
tara-pogancev <tara.pogancev@levi9.com>
2024-06-07 14:00:49 +0200

    To the heavens, first functional pass with getting data from AWS Bedrock, and formatting a response with citations

--- re_searcher_api/consts.py
+++ re_searcher_api/consts.py
@@ -1,3 +1,8 @@
 openai_api_key = "sk-proj-ZsM6B1D7ug99052NWGs3T3BlbkFJLvHuR76qMC3LoAy5XT4c"
 
 api_password = "OfficeTimePassword"
+
+knowledge_base_id = "B7XUK2PH3W"
+
+aws_access_key_id = "AKIAW3MEATVE7LM6U34H"
+aws_secret_access_key = "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"
--- re_searcher_api/core/document_retrival_service.py
+++ re_searcher_api/core/document_retrival_service.py
@@ -1,4 +1,32 @@
+import boto3
+
+from consts import knowledge_base_id, aws_access_key_id, aws_secret_access_key
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+knowledge_base_client = session.client(service_name="bedrock-agent-runtime")
+
+
 def get_document_citations(user_message, document_file_name):
     citations = []
+
+    response = knowledge_base_client.retrieve(
+        knowledgeBaseId=knowledge_base_id,
+        retrievalQuery={
+            'text': user_message
+        },
+        retrievalConfiguration={
+            'vectorSearchConfiguration': {
+                'numberOfResults': 5,
+                # 'overrideSearchType': "HYBRID",
+            }
+        }
+    )
+
+    for retrievedResult in response["retrievalResults"]:
+        citations.append(retrievedResult['content']['text'])
 
     return citations
